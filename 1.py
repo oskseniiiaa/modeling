3f0249b9011d96db7ab9cb36e86b131dfe7da296
oskseniiiaa <zemkat190287@gmail.com>
2024-10-03 15:58:07 +0000

    lec1

--- 1.py
+++ 1.py
@@ -1,1 +1,24 @@
+#print(3+4)
+#print(4//3)
+#S=3+4
+#memory_id =id(S)
+#print(S , memory_id)
+#S=5
+#print(S)
+#type(3)
+#print(type(3))
+#print(type(3.4))
+#print(type"Молодец"))
+#a = "molodec"
+#print(type(a))
+#a = ["molodec" ,1, "круто", 6,8]
+#print(type(True))
+#print(print(False))
+#a = input
+#print(a)
+#print(type(a))
+#a = int(input('Введите значение a:'))
 
+#print('Hello Word')
+#print('Маша + Петя = Любовь')
+#print(3+4)
